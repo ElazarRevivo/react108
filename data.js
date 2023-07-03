a8ee775cc7c60844bc010da4208aeb4deebb0365
ElazarRevivo <138401704+ElazarRevivo@users.noreply.github.com>
2023-07-04 02:57:34 +0300

    Update data.js

--- data.js
+++ data.js
@@ -17,24 +17,6 @@ function sendDataToReact(altitude, his, adi) {
    .catch(error => {
      console.error('Error fetching data:', error);
    });
-  // fetch("localhost:3001/test",{
-  //   method: "POST",
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   },
-  //   body: data
-  // }).then(data=>{console.log(data)})
-
-  // const req = http.request(options, (res) => {
-  //   console.log(`Data sent to React with status code: ${res.statusCode}`);
-  // });
-
-  // req.on('error', (error) => {
-  //   console.error(`Error sending data to React: ${error}`);
-  // });
-
-  // req.write(data);
-  // req.end();
 }
 
 rl.question('Enter Altitude (0-3000): ', (altitude) => {
